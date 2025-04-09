e613019879fecff15d56187160783ec26f22b94b
adithya876 <nimsarav422@gmail.com>
2025-04-09 23:37:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Movie-MD&VGEWzJoT#q2Cd9MQq2XpQVzIhvAn1g7s8aXfEEgJOLpxnvDESwLE' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Movie-MD&JDVkABLT#6buaCHjCHgCPMjK6_6pgnqn92uIMI74qNCaNOmOus3E' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 BUTTON: process.env.PORT === undefined ? "true" : process.env.PORT,
